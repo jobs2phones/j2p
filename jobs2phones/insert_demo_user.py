856b66fbe82bf2216ecd8a9cc91c5cd8cdb35e81
Stephen Suffian <stephen.suffian@gmail.com>
2014-12-11 21:14:04 -0500

    edited execution to use multiple search terms from the config file

--- jobs2phones/insert_demo_user.py
+++ jobs2phones/insert_demo_user.py
@@ -7,4 +7,4 @@ search_term='warehouse&20philadelphia'
 
 Session = load.bind_to_database(cf['postgres_username'],
         cf['postgres_password'],cf['postgres_db'])
-load.insert_user(Session,'',cf['toaddrs_demo'],'pword',search_term)
+load.insert_user(Session,'',cf['toaddrs_demo'],'password',search_term)
